6812d711262bdcc274a230560ebf0553735ac490
yns94190 <yaniswalim@gmail.com>
2026-09-16 22:27:46 +0200

    feat: add first dockerized python app

---
+++ semaine-02/app/app.py
@@ -0,0 +1,9 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+class Handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b"Hello from Docker!")
+
+HTTPServer(("", 8080), Handler).serve_forever()
